0a66f106ec56c28d03635f64436ef1691cd65022
aurakanzaa <aurakanzaa@yahoo.co.id>
2019-03-05 07:55:46 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# simulasiPegadaian-front
---
+++ application/views/home.php
@@ -0,0 +1,268 @@
+<!-- Banner Area Starts -->
+<!-- <section class="banner-area">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-6 px-0">
+                <div class="banner-bg"></div>
+            </div>
+            <div class="col-lg-6 align-self-center">
+                <div class="banner-text">
+                    <h1>find your dream <span>job</span> with comport</h1>
+                    <p class="py-3">Wherein herb beginning. Moved after gathering. Sea hi crate fowl man replenish place divided likeness herb one two lnetn Winged moving saw, may over.</p>
+                    <a href="#" class="secondary-btn">explore now<span class="flaticon-next"></span></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section> -->
+<!-- Banner Area End -->
+
+<!-- Search Area Starts -->
+<!-- <div class="search-area">
+    <div class="search-bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <form action="#" class="d-md-flex justify-content-between">
+                        <select>
+                            <option value="1">All Category</option>
+                            <option value="2">Part Time</option>
+                            <option value="3">Full Time</option>
+                            <option value="4">Remote</option>
+                            <option value="5">Office Job</option>
+                        </select>
+                        <select>
+                            <option value="1">Select Location</option>
+                            <option value="2">Dhaka</option>
+                            <option value="3">Rajshahi</option>
+                            <option value="4">Barishal</option>
+                            <option value="5">Noakhali</option>
+                        </select>
+                        <input type="text" placeholder="Search Keyword" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Search Keyword'" required>
+                        <button type="submit" class="template-btn">find job</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> -->
+<!-- Search Area End -->
+
+<!-- Feature Area Starts -->
+<!-- <section class="feature-area section-padding1">
+    <div class="container">
+        <div align="center">
+            <h1 style="color:#04091e">find your menu below</h1>
+        </div>
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="c1 single-feature mb-4 mb-lg-0">
+                    <h2 style="color:white;">Reguler</h2>
+                    <p class="py-3">There spirit beginning bearing the open at own every give appear in third you sawe two boys</p>
+                    <a href="#" class="secondary-btn">explore now<span class="flaticon-next"></span></a>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="c1 single-feature mb-4 mb-lg-0">
+                    <h2>Fleksi</h2>
+                    <p class="py-3">There spirit beginning bearing the open at own every give appear in third you sawe two boys</p>
+                    <a href="#" class="secondary-btn">explore now<span class="flaticon-next"></span></a>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="c1 single-feature">
+                    <h2>Fleksi Berjangka</h2>
+                    <p class="py-3">There spirit beginning bearing the open at own every give appear in third you sawe two boys</p>
+                    <a href="#" class="secondary-btn">explore now<span class="flaticon-next"></span></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section> -->
+<br><br>
+<!-- Feature Area End -->
+
+<section class="category-area section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="section-top text-center">
+                        <h2>Find job by category</h2>
+                        <p>Open lesser winged midst wherein may morning</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3 col-md-6">
+                    <div class="single-category text-center mb-4">
+                        <img src="assets/images/cat1.png" alt="category">
+                        <h4>accounting & Finance</h4>
+                        <h5>250 open job</h5>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="single-category text-center mb-4">
+                        <img src="assets/images/cat2.png" alt="category">
+                        <h4>production & operation</h4>
+                        <h5>250 open job</h5>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="single-category text-center mb-4">
+                        <img src="assets/images/cat3.png" alt="category">
+                        <h4>telecommunication</h4>
+                        <h5>250 open job</h5>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="single-category text-center mb-4">
+                        <img src="assets/images/cat4.png" alt="category">
+                        <h4>garments & textile</h4>
+                        <h5>250 open job</h5>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="single-category text-center mb-4 mb-lg-0">
+                        <img src="assets/images/cat5.png" alt="category">
+                        <h4>marketing and sales</h4>
+                        <h5>250 open job</h5>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="single-category text-center mb-4 mb-lg-0">
+                        <img src="assets/images/cat6.png" alt="category">
+                        <h4>engineer & architech</h4>
+                        <h5>250 open job</h5>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="single-category text-center mb-4 mb-md-0">
+                        <img src="assets/images/cat7.png" alt="category">
+                        <h4>design & crative</h4>
+                        <h5>250 open job</h5>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="single-category text-center">
+                        <img src="assets/images/cat8.png" alt="category">
+                        <h4>customer support</h4>
+                        <h5>250 open job</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+<!-- Newsletter Area Starts -->
+<!-- <section class="newsletter-area section-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="section-top text-center">
+                    <h2>Get job information daily</h2>
+                    <p>Subscribe to our newsletter and get a coupon code!</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <form action="#">
+                    <input type="email" placeholder="Your email here" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Your email here'" required>
+                    <button type="submit" class="template-btn">subscribe now</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</section> -->
+<!-- Newsletter Area End -->
+
+<!-- Employee Area Starts -->
+<!-- <section class="employee-area section-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="section-top text-center">
+                    <h2>Happy employee</h2>
+                    <p>Open lesser winged midst wherein may morning</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="employee-slider owl-carousel">
+                    <div class="single-slide d-sm-flex">
+                        <div class="slide-img employee1">
+                            <div class="hover-state">
+                                <div class="hover-text text-center">
+                                    <h3>david aron</h3>
+                                    <h5>Facebook</h5>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="slide-text align-self-center">
+                            <i class="fa fa-quote-left"></i>
+                            <p>Appear, called day. Sixth two after eve moved called winged very heaven two lights let all third gathered.</p>
+                        </div>
+                    </div>
+                    <div class="single-slide d-sm-flex">
+                        <div class="slide-img employee2">
+                            <div class="hover-state">
+                                <div class="hover-text text-center">
+                                    <h3>david aron</h3>
+                                    <h5>Twitter</h5>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="slide-text align-self-center">
+                            <i class="fa fa-quote-left"></i>
+                            <p>Appear, called day. Sixth two after eve moved called winged very heaven two lights let all third gathered.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section> -->
+<!-- Employee Area End -->
+
+
+
+<!-- Download Area Starts -->
+<!-- <section class="download-area section-padding">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="download-text">
+                    <h2>Download the app your mobile today</h2>
+                    <p class="py-3">Light earth also land can't. May you midst shall lights blessed in lights Have gathered image morning blessed grass him. Appear female rule all seas she'd winged</p>
+                    <div class="download-button d-sm-flex flex-row justify-content-start">
+                        <div class="download-btn mb-3 mb-sm-0 flex-row d-flex">
+                            <i class="fa fa-apple" aria-hidden="true"></i>
+                            <a href="#">
+                                <p>
+                                    <span>Available</span> <br>
+                                    on App Store
+                                </p>
+                            </a>
+                        </div>
+                        <div class="download-btn dark flex-row d-flex">
+                            <i class="fa fa-android" aria-hidden="true"></i>
+                            <a href="#">
+                                <p>
+                                    <span>Available</span> <br>
+                                    on Play Store
+                                </p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 pr-0">
+                <div class="download-img"></div>
+            </div>
+        </div>
+    </div>
+</section> -->
+<!-- Download Area End -->
